92fd64db124a2ccd93275e907daef48c69c02407
Alikhan <73962071@continental.edu.pe>
2021-08-17 22:31:41 -0500

    return to the mainactivity

--- app/src/main/java/com/paparazziteam/marketshop/Activities/ProductDetailsActivity.kt
+++ app/src/main/java/com/paparazziteam/marketshop/Activities/ProductDetailsActivity.kt
@@ -2,17 +2,27 @@ package com.paparazziteam.marketshop.Activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.paparazziteam.marketshop.R
+import com.paparazziteam.marketshop.databinding.ActivityProductDetailsBinding
 
 class ProductDetailsActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityProductDetailsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_product_details)
+        binding = ActivityProductDetailsBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
-           var code = intent.getStringExtra("CODE_RESULT")
+        var code = intent.getStringExtra("CODE_RESULT")
 
-            android.util.Log.i("CODE_RESULT","CODIGO: $code")
+        binding.backImage.setOnClickListener(View.OnClickListener {
+
+            finish()
+        })
+
 
 
     }
