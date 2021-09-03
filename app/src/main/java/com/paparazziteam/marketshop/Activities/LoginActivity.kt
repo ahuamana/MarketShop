62ba0631458a84744233c71874538f1a54eda8c2
Alikhan <73962071@continental.edu.pe>
2021-09-03 12:20:07 -0500

    change between intents

--- app/src/main/java/com/paparazziteam/marketshop/Activities/LoginActivity.kt
+++ app/src/main/java/com/paparazziteam/marketshop/Activities/LoginActivity.kt
@@ -1,12 +1,37 @@
 package com.paparazziteam.marketshop.Activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.paparazziteam.marketshop.R
+import com.paparazziteam.marketshop.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+
+        getClickListener()
+
+    }
+
+    private fun getClickListener() {
+
+        binding.loginAnonymous.setOnClickListener {
+            val intent = Intent(this@LoginActivity, MainActivity::class.java)
+            //intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+        }
+
+        binding.loginEmail.setOnClickListener {
+
+        }
+
     }
 }
--- app/src/main/java/com/paparazziteam/marketshop/Activities/MainActivity.kt
+++ app/src/main/java/com/paparazziteam/marketshop/Activities/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.paparazziteam.marketshop.Activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -93,6 +94,14 @@ class MainActivity : AppCompatActivity() {
     }
 
 
+    override fun onBackPressed() {
+        super.onBackPressed()
+
+        val intent = Intent(this@MainActivity, LoginActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+        startActivity(intent)
+
+    }
 
 
 }
