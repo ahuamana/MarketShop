f333ceac4e5fc77b5015a21de18a941997bd019e
Alikhan <73962071@continental.edu.pe>
2021-09-06 10:20:12 -0500

    check user exist

--- app/src/main/java/com/paparazziteam/marketshop/Activities/LoginEmailActivity.kt
+++ app/src/main/java/com/paparazziteam/marketshop/Activities/LoginEmailActivity.kt
@@ -2,12 +2,16 @@ package com.paparazziteam.marketshop.Activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.paparazziteam.marketshop.Providers.UserProvider
 import com.paparazziteam.marketshop.R
 import com.paparazziteam.marketshop.databinding.ActivityLoginEmailBinding
 
 class LoginEmailActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginEmailBinding
+
+    var mUserProvider = UserProvider()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,7 +51,22 @@ class LoginEmailActivity : AppCompatActivity() {
         {
             if(!user.equals(""))
             {
+                if(pass != null)
+                {
+                    if(!pass.equals(""))
+                    {
 
+                        mUserProvider.getUserInfo(user).get().addOnCompleteListener {
+                            if(it.isSuccessful)
+                            {
+
+                               Log.e("DATA","user: ${it.result!!.data!!["username"]}")
+                               Log.e("DATA","password: ${it.result!!.data!!["password"]}")
+                            }
+                        }
+
+                    }
+                }
             }
         }
 
