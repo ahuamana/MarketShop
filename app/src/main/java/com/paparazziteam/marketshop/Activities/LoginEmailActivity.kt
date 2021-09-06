49940deae29ebd9b7a786a7af62d5e0f2a6be3ff
Alikhan <73962071@continental.edu.pe>
2021-09-06 11:11:40 -0500

    login correctly

--- app/src/main/java/com/paparazziteam/marketshop/Activities/LoginEmailActivity.kt
+++ app/src/main/java/com/paparazziteam/marketshop/Activities/LoginEmailActivity.kt
@@ -3,8 +3,9 @@ package com.paparazziteam.marketshop.Activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import com.paparazziteam.marketshop.Providers.AuthProvider
 import com.paparazziteam.marketshop.Providers.UserProvider
-import com.paparazziteam.marketshop.R
 import com.paparazziteam.marketshop.databinding.ActivityLoginEmailBinding
 
 class LoginEmailActivity : AppCompatActivity() {
@@ -12,6 +13,7 @@ class LoginEmailActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginEmailBinding
 
     var mUserProvider = UserProvider()
+    var mAuth = AuthProvider()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -56,17 +58,53 @@ class LoginEmailActivity : AppCompatActivity() {
                     if(!pass.equals(""))
                     {
 
-                        mUserProvider.getUserInfo(user).get().addOnCompleteListener {
-                            if(it.isSuccessful)
+                        mUserProvider.getUserInfo(user).get().addOnSuccessListener {
+                            if(it.exists())
                             {
 
-                               Log.e("DATA","user: ${it.result!!.data!!["username"]}")
-                               Log.e("DATA","password: ${it.result!!.data!!["password"]}")
+                                var us=it.data!!.get("username")
+                                var pa=it.data!!.get("password")
+
+                                if(us!!.equals(user) && pa!!.equals(pass))
+                                {
+                                    //Login
+                                    loginEmail(user,pass)
+
+                                }else
+                                {
+                                    Toast.makeText(this@LoginEmailActivity,"Correo o Contraseña Incorrecto!",Toast.LENGTH_SHORT).show()
+                                }
+
+                                //Log.e("DATA", "user: ${it.data}")
+                                //Log.e("DATA","user: ${it.data!!.get("username")}")
+                                //Log.e("DATA","pass: ${it.data!!.get("password")}")
+
+                            }else
+                            {
+                                Toast.makeText(this@LoginEmailActivity,"Usuario No Registrado",Toast.LENGTH_SHORT).show()
                             }
                         }
 
                     }
                 }
+            }
+        }
+
+    }
+
+    private fun loginEmail(user: String, pass: String) {
+
+        mAuth.login(user, pass).addOnCompleteListener { task->
+            if (task.isSuccessful) {
+                // Sign in success, update UI with the signed-in user's information
+                Log.e("TAG", "Iniciando Session!")
+                //val user = auth.currentUser
+                //updateUI(user)
+            } else {
+                // If sign in fails, display a message to the user.
+                Log.w("TAG", "signInWithEmail:failure", task.exception)
+                Toast.makeText(baseContext, "Error, al iniciar session", Toast.LENGTH_SHORT).show()
+
             }
         }
 
