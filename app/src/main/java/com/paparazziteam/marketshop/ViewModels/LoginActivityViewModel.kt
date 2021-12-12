a2cc8fda8d9612632cf68b009e04ea8da351c094
Alikhan <73962071@continental.edu.pe>
2021-12-12 09:49:18 -0500

    implemented login viewmodel

--- app/src/main/java/com/paparazziteam/marketshop/ViewModels/LoginActivityViewModel.kt
+++ app/src/main/java/com/paparazziteam/marketshop/ViewModels/LoginActivityViewModel.kt
@@ -4,11 +4,17 @@ import android.content.Context
 import android.content.Intent
 import android.util.Log
 import androidx.lifecycle.ViewModel
+import com.paparazziteam.marketshop.Activities.LoginEmailActivity
 import com.paparazziteam.marketshop.Activities.MainActivity
 import com.paparazziteam.marketshop.Providers.AuthProvider
+import com.paparazziteam.marketshop.databinding.ActivityLoginBinding
 
-class LoginActivityViewModel(private var mAuthProvider: AuthProvider,private val context: Context) :ViewModel() {
+class LoginActivityViewModel(private var mAuthProvider: AuthProvider,private val context: Context,private val binding: ActivityLoginBinding) :ViewModel() {
 
+
+    init {
+        clickListeners()
+    }
 
     fun checkUserLoginAlready()
     {
@@ -25,6 +31,23 @@ class LoginActivityViewModel(private var mAuthProvider: AuthProvider,private val
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
             context.startActivity(intent)
         }
+
+    }
+
+    fun clickListeners()
+    {
+            binding.loginAnonymous.setOnClickListener {
+                val intent = Intent(context, MainActivity::class.java).apply {
+                    putExtra("username", "")
+                }
+                //intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+                context.startActivity(intent)
+            }
+
+            binding.loginEmail.setOnClickListener {
+                val intent = Intent(context, LoginEmailActivity::class.java)
+                context.startActivity(intent)
+            }
 
     }
 
